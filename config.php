c110b021db5a34c3649c9a25ef1ad22c7343cb09
ChoiJengYee <167624072+ChoiJengYee@users.noreply.github.com>
2024-06-15 22:39:24 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,3 +1,4 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', '', 'user_db') or die('Connection failed: ' . mysqli_connect_error());
+$conn = mysqli_connect('localhost', 'id22311917_tt6l05', 'TT6l-051234', 'id22311917_frontend') 
+or die('Connection failed: ' . mysqli_connect_error());
 ?>
